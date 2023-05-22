3fc14ec9d1ce4361421aa9872ee5645067122646
Daniil Hryshko <hryshkod@gmail.com>
2023-05-22 17:51:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,17 +15,37 @@ PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o 
 PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o ens4 -j MASQUERADE
 '''
 
+ip_ch = 2
+
 for client in ["Alpha", "Beta", "Gamma", "Delta", "Epsilon", "Zeta", "Eta"]:
     client_public_key = open(f'/etc/wireguard/{client}/publickey').read().strip()
-    client_private_key = open(f'/etc/wireguard/{client}/publickey').read().strip()
+    client_private_key = open(f'/etc/wireguard/{client}/privatekey').read().strip()
     peer_to_server = f'''
 
 [Peer]
 PublicKey = {client_public_key}
-AllowedIPs = 10.0.0.2/32 
+AllowedIPs = 10.0.0.{ip_ch}/32 
 
 '''
     wb_conf += peer_to_server
+
+    peer_to_client = f'''
+    [Interface]
+    PrivateKey = {client_private_key}
+    Address = 10.0.0.{ip_ch}/32
+    DNS = 8.8.8.8
+
+    [Peer]
+    PublicKey = {server_public_key}
+    Endpoint = {external_ip}:51850
+    AllowedIPs = 0.0.0.0/0
+    PersistentKeepalive = 15
+    '''
+
+    key_for_client = open(f"/root/setup-wireguard/client/{client}.conf", "w").write(peer_to_client)
+    ip_ch += 1
+
+server_conf = open('/etc/wireguard/wg0.conf', "w").write(wb_conf)
 
     peer_to_client = f'''
 [Interface]
@@ -40,6 +60,5 @@ AllowedIPs = 0.0.0.0/0
 PersistentKeepalive = 15
 '''
     key_for_client = open(f"/root/auto-sutup-wireguard/client/{client}.conf", "w").write(peer_to_client)
-
 
 server_conf = open('/etc/wireguard/wg0.conf', "w").write(wb_conf)
